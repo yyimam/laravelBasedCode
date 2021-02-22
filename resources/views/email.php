559c3d30fdbe716b1d319a560d0f858caf7427b3
name <nehal@embex.io>
2021-02-22 15:07:50 +0500

    heading change in email_verification mail

--- resources/views/email.php
+++ resources/views/email.php
@@ -1,4 +1,4 @@
-<h1>Password Reset Link</h1>
+<h1>Email Verifcation</h1>
 <br>
 <p>It looks like someone submitted a request to verify it's barebone account from this email. If it wasn’t you there's nothing to do nor worry about. You can keep on keeping on.
 If this was you, <a href="<?php echo $details['link'] ?>">Click Here</a> to verify your email…</p>
